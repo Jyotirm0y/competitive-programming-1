5812d424c7628b8e975d0dde0cfa658c91d87391
Rodrigo Alves <rodrigovieira1994@gmail.com>
2013-11-02 15:32:50 -0300

    one more

---
+++ egypt-11854.cpp
@@ -0,0 +1,26 @@
+/**
+author: Rodrigo Alves
+problem: Egypt
+url: http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=2954
+date: November 2
+**/
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+
+    while(scanf("%d %d %d", &a, &b, &c), (a != 0 || b != 0 || c != 0)) {
+        if (a * a == b*b + c*c ||
+            b * b == a*a + c*c ||
+            c * c == a*a + b*b
+            ) {
+            printf("%s\n", "right");
+        } else {
+            printf("%s\n", "wrong");
+        }
+    }
+
+    return 0;
+}
