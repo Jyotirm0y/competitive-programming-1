a698d92fe7821be10835f6cb8337069891ee3f61
Rodrigo Alves <rodrigovieira1994@gmail.com>
2013-11-11 19:57:04 -0200

    one more

---
+++ hs-physics-10071.cpp
@@ -0,0 +1,16 @@
+// author: Rodrigo Alves
+// problem: Back to High School Physics
+// url: http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=1012
+// status: AC
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+
+    while (scanf("%d %d", &a, &b) != EOF) {
+        printf("%d\n", 2 * (a * b));
+    }
+
+    return 0;
+}
