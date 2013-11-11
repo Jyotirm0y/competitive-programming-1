16989139b8147c9fc6c625bbe13cbe2dc948c353
Rodrigo Alves <rodrigovieira1994@gmail.com>
2013-11-11 21:26:54 -0200

    solving one more. closes #62

---
+++ bafana-11805.cpp
@@ -0,0 +1,23 @@
+// author: Rodrigo Alves
+// problem: Bafana Bafana
+// url: http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=2905
+// status: AC
+#include <stdio.h>
+
+int numbers[30];
+
+int main()
+{
+    int T, N, K, P, total, cas = 1;
+    scanf("%d", &T);
+
+    while (T--) {
+        total = 0;
+        scanf("%d %d %d", &N, &K, &P);
+        for (int i = 1; i <= N; i++) numbers[i-1] = i;
+
+        printf("Case %d: %d\n", cas++, (P+K-1) %N+1);
+    }
+
+    return 0;
+}
