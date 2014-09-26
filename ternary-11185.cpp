2cbad981b8dfa4d0a34f5b6b45bd903cfd7d9884
Rodrigo Alves <rodrigovieira1994@gmail.com>
2014-09-26 10:50:23 -0300

    working on #112

---
+++ ternary-11185.cpp
@@ -0,0 +1,33 @@
+// author: Rodrigo Alves
+// problem: Ternary
+// url: http://uva.onlinejudge.org/index.php?option=onlinejudge&Itemid=8&category=23&page=show_problem&problem=2126
+// status: WA
+
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+int main()
+{
+  long long N, rem;
+
+  while (scanf("%lld", &N), N >= 0) {
+    vector<long long> list;
+
+    while (N > 0) {
+      rem = N % 3;
+      N = N / 3;
+
+      list.push_back(rem);
+    }
+
+    for (int i = list.size() - 1; i >= 0; i--) {
+      printf("%lld", list[i]);
+    }
+
+    printf("\n");
+    list.clear();
+  }
+
+  return 0;
+}
