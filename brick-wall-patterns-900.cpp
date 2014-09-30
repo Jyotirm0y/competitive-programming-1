be575b278d3887cfe414d1a1b462eb256ebd3a78
Rodrigo Alves <rodrigovieira1994@gmail.com>
2014-09-30 01:22:15 -0300

    One more, Fibonacci joy. Closes #154

---
+++ brick-wall-patterns-900.cpp
@@ -0,0 +1,25 @@
+// author: Rodrigo Alves
+// problem: Brick Wall Patterns
+// url: http://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=841
+// status: AC
+#include <cstdio>
+
+long long table[51];
+
+void walls(int n)
+{
+  table[0] = table[1] = 1;
+  for (int i = 2; i < n; i++) table[i] = table[i-1] + table[i-2];
+}
+
+int main()
+{
+  walls(51);
+  int N;
+
+  while (scanf("%d", &N), N != 0) {
+    printf("%lld\n", table[N]);
+  }
+
+  return 0;
+}
