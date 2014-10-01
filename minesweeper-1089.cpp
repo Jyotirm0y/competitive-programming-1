575e7f9549d1303a2ba3ac66b6f54c76dca624a6
Rodrigo Alves <rodrigovieira1994@gmail.com>
2014-09-30 23:36:13 -0300

    Finishing another one :smirk:. Closes #155

--- minesweeper-1089.cpp
+++ minesweeper-1089.cpp
@@ -1,21 +1,20 @@
 // author: Rodrigo Alves
 // problem: Minesweeper
 // url: http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem=1130
-// status: WA
+// status: AC
 #include <cstdio>
 
-char matrix[102][102];
+char matrix[110][110];
 
 int main()
 {
-  freopen("input.txt", "r", stdin);
-  freopen("output.txt", "w", stdout);
   int N, M, counter = 1, inst = 0;
 
   while (scanf("%d %d", &N, &M), N != 0 && M != 0) {
     int answer[102][102] = {0};
 
-    for (int i = 0; i < N; i++) scanf(" %s", matrix[i]);
+    for (int i = 0; i < N; i++) scanf("%s", matrix[i]);
+
     if (inst == 1) printf("\n");
     inst = 1;
 
@@ -23,15 +22,17 @@ int main()
 
     for (int i = 0; i < N; i++) {
       for (int j = 0; j < M; j++) {
-        if (matrix[i][j] == '*') {
-          answer[i-1][j-1] += 1;
-          answer[i-1][j] += 1;
-          answer[i-1][j+1] += 1;
 
-          answer[i][j-1] += 1;
+        if (matrix[i][j] == '*') {
+
+          if (i != 0 && j != 0) answer[i-1][j-1] += 1;
+          if (i != 0) answer[i-1][j] += 1;
+          if (i != 0) answer[i-1][j+1] += 1;
+
+          if (j != 0) answer[i][j-1] += 1;
           answer[i][j+1] += 1;
 
-          answer[i+1][j-1] += 1;
+          if (j != 0) answer[i+1][j-1] += 1;
           answer[i+1][j] += 1;
           answer[i+1][j+1] += 1;
         }
