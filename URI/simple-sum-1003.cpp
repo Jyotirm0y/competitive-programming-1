98e8520f19cd3cd94c79888d7161c2f07d3519cc
Rodrigo Alves <rodrigovieira1994@gmail.com>
2014-11-13 23:43:17 -0300

    one more

---
+++ URI/simple-sum-1003.cpp
@@ -0,0 +1,24 @@
+// author: Rodrigo Alves
+// Judge: URI
+// problem: Simple Sum
+// url: https://www.urionlinejudge.com.br/judge/en/problems/view/1003
+// status: AC
+#include <cstdio>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+  int A, B;
+  scanf("%d", &A);
+  scanf("%d", &B);
+
+  printf("SOMA = %d\n", A + B);
+
+  return 0;
+}
