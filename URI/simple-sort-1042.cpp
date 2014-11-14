3c4a9bbe954f541f575ddec84f10d4036e91e5fb
Rodrigo Alves <rodrigovieira1994@gmail.com>
2014-11-14 02:55:27 -0300

    fixing this one

--- URI/simple-sort-1042.cpp
+++ URI/simple-sort-1042.cpp
@@ -17,16 +17,25 @@ int main()
   int A, B, C;
   scanf("%d %d %d", &A, &B, &C);
 
+  vector<int> original;
   vector<int> numbers;
+
+  original.push_back(A);
   numbers.push_back(A);
+
+  original.push_back(B);
   numbers.push_back(B);
+
+  original.push_back(C);
   numbers.push_back(C);
 
   sort(numbers.begin(), numbers.end());
 
-  for (int i = 0; i < 3; i++) {
-    printf("%d\n", numbers[i]);
-  }
+  for (int i = 0; i < 3; i++) printf("%d\n", numbers[i]);
+
+  printf("\n");
+
+  for (int i = 0; i < 3; i++) printf("%d\n", original[i]);
 
   return 0;
 }
