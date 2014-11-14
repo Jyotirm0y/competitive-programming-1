102d39b883fbd98ab4f7bee7d76d0dd97d201c52
Rodrigo Alves <rodrigovieira1994@gmail.com>
2014-11-13 23:45:09 -0300

    one more

---
+++ URI/simple-product-1004.cpp
@@ -0,0 +1,24 @@
+// author: Rodrigo Alves
+// Judge: URI
+// problem: Simple Product
+// url: https://www.urionlinejudge.com.br/judge/en/problems/view/1004
+// status: AC
+#include <cstdio>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+  int A, B;
+  scanf("%d", &A);
+  scanf("%d", &B);
+
+  printf("PROD = %d\n", A * B);
+
+  return 0;
+}
