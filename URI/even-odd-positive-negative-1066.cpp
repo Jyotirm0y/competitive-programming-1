0bcb0a290788b79ecf02628bd380cf29174a720f
Rodrigo Alves <rodrigovieira1994@gmail.com>
2014-11-14 05:18:47 -0300

    one more. probably the more stupid valid answer I ever had. this is like killing a fly with a bazooka

---
+++ URI/even-odd-positive-negative-1066.cpp
@@ -0,0 +1,39 @@
+// author: Rodrigo Alves
+// judge: URI
+// problem: Even, Odd, Positive and Negative
+// url: https://www.urionlinejudge.com.br/judge/en/problems/view/1066
+// status: AC
+#include <cstdio>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+  int n1, n2, n3, n4, n5, even_numbers = 0, positives = 0, zeroes = 0;
+  scanf("%d %d %d %d %d", &n1, &n2, &n3, &n4, &n5);
+  vector<int> numbers;
+
+  numbers.push_back(n1);
+  numbers.push_back(n2);
+  numbers.push_back(n3);
+  numbers.push_back(n4);
+  numbers.push_back(n5);
+
+  for (int i = 0; i < 5; i++) {
+    if (numbers[i] % 2 == 0) even_numbers++;
+    if (numbers[i] > 0) positives++;
+    if (numbers[i] == 0) zeroes++;
+  }
+
+  printf("%d %s\n", even_numbers, "valor(es) par(es)");
+  printf("%d %s\n", 5 - even_numbers, "valor(es) impar(es)");
+  printf("%d %s\n", positives, "valor(es) positivo(s)");
+  printf("%d %s\n", 5 - positives - zeroes, "valor(es) negativo(s)");
+
+  return 0;
+}
