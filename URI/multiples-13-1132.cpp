ec3048516c7d3cf60fd10c8759d0d3f535078032
Rodrigo Alves <rodrigovieira1994@gmail.com>
2014-11-14 12:22:57 -0300

    one more

---
+++ URI/multiples-13-1132.cpp
@@ -0,0 +1,35 @@
+// author: Rodrigo Alves
+// judge: URI
+// problem: Multiples of 13
+// url: https://www.urionlinejudge.com.br/judge/en/problems/view/1132
+// status: AC
+#include <cstdio>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+  int X, Y, start, end, answer = 0;
+  scanf("%d %d", &X, &Y);
+
+  if (X > Y) {
+    start = Y;
+    end = X;
+  } else {
+    start = X;
+    end = Y;
+  }
+
+  for (int i = start; i <= end; i++) {
+    if (i % 13 != 0) answer += i;
+  }
+
+  printf("%d\n", answer);
+
+  return 0;
+}
