58ce3f8c541e81c46a7ef68495c8c4187310bfc8
Rodrigo Alves <rodrigovieira1994@gmail.com>
2014-11-15 20:51:26 -0300

    First one from SPOJ. Closes #240

---
+++ SPOJ/life-universe-everything.cpp
@@ -0,0 +1,29 @@
+// url: http://www.spoj.com/problems/TEST/
+
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	bool found = false;
+
+	while (scanf("%d", &N) && !found) {
+		if (N != 42) {
+			printf("%d\n", N);
+		} else {
+			found = true;
+		}
+	}
+
+	return 0;
+}
