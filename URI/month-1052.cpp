efe1772f2c053dab0ff606f50a2cb812badc1362
Rodrigo Alves <rodrigovieira1994@gmail.com>
2014-11-15 20:33:03 -0300

    more

---
+++ URI/month-1052.cpp
@@ -0,0 +1,39 @@
+// author: Rodrigo Alves
+// judge: URI
+// problem: Month
+// url: https://www.urionlinejudge.com.br/judge/en/problems/view/1052
+// status: AC
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int N;
+	scanf("%d", &N);
+
+	vector<string> words;
+	words.push_back("January");
+	words.push_back("February");
+	words.push_back("March");
+	words.push_back("April");
+	words.push_back("May");
+	words.push_back("June");
+	words.push_back("July");
+	words.push_back("August");
+	words.push_back("September");
+	words.push_back("October");
+	words.push_back("November");
+	words.push_back("December");
+
+	cout << words[N-1] << endl;
+
+	return 0;
+}
