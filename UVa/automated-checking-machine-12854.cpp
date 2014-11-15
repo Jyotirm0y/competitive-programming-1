c41eb93aba39b919b29f23fec116ee9e0ccbd1b5
Rodrigo Alves <rodrigovieira1994@gmail.com>
2014-11-15 20:41:55 -0300

    one more from uva

---
+++ UVa/automated-checking-machine-12854.cpp
@@ -0,0 +1,40 @@
+// author: Rodrigo Alves
+// problem: Automatic Checking Machine
+// url: http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=4719
+// type: Ad hoc
+// status: AC
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <cmath>
+
+using namespace std;
+
+int A[5], B[5];
+
+int main()
+{
+	while (scanf("%d", &A[0]) == 1) {
+		bool ok = true;
+
+		for (int i = 1; i < 5; i++) scanf("%d", &A[i]);
+		for (int i = 0; i < 5; i++) scanf("%d", &B[i]);
+
+		for (int i = 0; i < 5; i++) {
+			if (A[i] == B[i]) {
+				ok = false;
+				break;
+			}
+		}
+
+		if (ok) printf("Y\n");
+		else printf("N\n");
+	}
+
+	return 0;
+}
