67fdc54c16efce98b2ec224bb33fd903390b32ff
Rodrigo Alves <rodrigovieira1994@gmail.com>
2014-11-16 14:39:04 -0300

    one more from uri

---
+++ URI/simple-factorial-1153.cpp
@@ -0,0 +1,30 @@
+// author: Rodrigo Alves
+// judge: URI
+// problem: Simple Factorial
+// url: https://www.urionlinejudge.com.br/judge/en/problems/view/1153
+// status: AC
+#include <cstdio>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+#define MAX 15
+
+long long dp[MAX];
+
+int main()
+{
+  dp[0] = dp[1] = 1;
+  for (int i = 2; i < MAX; i++) dp[i] = i * dp[i-1];
+
+  int N;
+  scanf("%d", &N);
+
+  printf("%lld\n", dp[N]);
+
+  return 0;
+}
