0358718866c0b4b3143ee803758a955985e8b9ad
Rodrigo Alves <rodrigovieira1994@gmail.com>
2014-11-16 15:09:37 -0300

    more questions

---
+++ URI/DDD-1050.cpp
@@ -0,0 +1,31 @@
+// author: Rodrigo Alves
+// judge: URI
+// problem: DDD
+// url: https://www.urionlinejudge.com.br/judge/en/problems/view/1050
+// status: AC
+#include <cstdio>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+  int N;
+  scanf("%d", &N);
+
+  if (N == 61) printf("Brasilia\n");
+  else if (N == 71) printf("Salvador\n");
+  else if (N == 11) printf("Sao Paulo\n");
+  else if (N == 21) printf("Rio de Janeiro\n");
+  else if (N == 32) printf("Juiz de Fora\n");
+  else if (N == 19) printf("Campinas\n");
+  else if (N == 27) printf("Vitoria\n");
+  else if (N == 31) printf("Belo Horizonte\n");
+  else printf("DDD nao cadastrado\n");
+
+  return 0;
+}
---
+++ URI/dividing-x-y-1116.cpp
@@ -0,0 +1,32 @@
+// author: Rodrigo Alves
+// judge: URI
+// problem: Dividing X by Y
+// url: https://www.urionlinejudge.com.br/judge/en/problems/view/1116
+// status: AC
+#include <cstdio>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+  double N, X, Y;
+  scanf("%lf", &N);
+
+  while (N--) {
+    scanf("%lf %lf", &X, &Y);
+
+    if (Y == 0) {
+      printf("%s\n", "divisao impossivel");
+      continue;
+    } else {
+      printf("%.1lf\n", X / Y);
+    }
+  }
+
+  return 0;
+}
