9aaeaa4e52c31ed45478135dd8e3eed8745301e7
Rodrigo Alves <rodrigovieira1994@gmail.com>
2014-11-15 21:36:08 -0300

    one more. closes #242

---
+++ UVa/summing-digits-11332.cpp
@@ -0,0 +1,34 @@
+// author: Rodrigo Alves
+// judge: URI
+// problem: Summing Digits
+// url: http://uva.onlinejudge.org/external/113/11332.html
+// status: AC
+#include <cstdio>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+int f(int n) {
+  if (n < 10) return n;
+  int sum = 0;
+
+  while (n) {
+    sum += n % 10;
+    n /= 10;
+  }
+
+  return f(sum);
+}
+
+int main() {
+  int n;
+
+  while (scanf("%d", &n) == 1 && n) {
+    printf("%d\n", f(n));
+  }
+
+  return 0;
+}
