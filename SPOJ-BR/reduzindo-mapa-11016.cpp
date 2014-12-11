56f9f14f666b1cd8c9dffc6afc88f0ca85d16f82
Rodrigo Alves <rodrigovieira1994@gmail.com>
2014-12-11 03:24:52 -0300

    first attempt at #252

--- SPOJ-BR/reduzindo-mapa-11016.cpp
+++ SPOJ-BR/reduzindo-mapa-11016.cpp
@@ -2,29 +2,84 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Vertex {
-public:
-  int vertex;
-  int pathLength;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
 
-  Vertex() {};
-  Vertex(int v, int p): vertex(v), pathLength(p) {};
+class UnionFind {
+private:
+  vector<int> p, rank, setSize;
+  int numSets;
+
+public:
+  UnionFind(int N) {
+    setSize.assign(N, 1); numSets = N; rank.assign(N, 0); p.assign(N, 0);
+    for (int i = 0; i < N; i++) p[i] = i;
+  }
+
+  int findSet(int i) {
+    return (p[i] == i) ? i : (p[i] = findSet(p[i]));
+  }
+
+  bool isSameSet(int i, int j) {
+    return findSet(i) == findSet(j);
+  }
+
+  void unionSet(int i, int j) {
+    if (!isSameSet(i, j)) {
+      numSets--;
+      int x = findSet(i), y = findSet(j);
+
+      // rank is used to keep the tree short
+      if (rank[x] > rank[y]) {
+        p[y] = x; setSize[x] += setSize[y];
+      } else {
+        p[x] = y; setSize[y] += setSize[x];
+
+        if (rank[x] == rank[y]) rank[y]++;
+      }
+    }
+  }
+
+  int numDisjointSets() {
+    return numSets;
+  }
+
+  int sizeOfSet(int i) {
+    return setSize[findSet(i)];
+  }
 };
 
 int main()
 {
-  int N, M, U, V, C;
+  int mst_cost, N, M, U, V, C;
   scanf("%d", &N);
   scanf("%d", &M);
 
-  vector<vector<Vertex> > vertices(N, vector<Vertex>());
+  vector< pair<int, ii> > Edgelist;
 
   for (int i = 0; i < M; i++) {
     scanf("%d %d %d", &U, &V, &C);
-
-    Vertex vertex(V, C);
-    vertices[U-1].push_back(vertex);
+    Edgelist.push_back(make_pair(C, ii(U, V)));
   }
+
+  sort(Edgelist.begin(), Edgelist.end());
+
+  mst_cost = 0;
+  UnionFind UF(V);
+
+  for (int i = 0; i < M; i++) {
+    if (UF.numDisjointSets() == 1) break;
+
+    pair<int, ii> front = Edgelist[i];
+
+    if (!UF.isSameSet(front.second.first, front.second.second)) {
+      mst_cost += front.first;
+      UF.unionSet(front.second.first, front.second.second);
+    }
+  }
+
+  printf("%d\n", mst_cost);
 
   return 0;
 }
