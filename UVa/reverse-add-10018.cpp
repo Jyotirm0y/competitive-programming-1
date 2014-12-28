c4e71b9428abe9154641cb1a318bed1d9d788179
Rodrigo Alves <rodrigovieira1994@gmail.com>
2014-12-28 20:51:46 -0300

    starting #321

---
+++ UVa/reverse-add-10018.cpp
@@ -0,0 +1,44 @@
+// author: Rodrigo Alves
+// problem: Reverse and Add
+// url: http://uva.onlinejudge.org/external/100/10018.html
+// status: AC
+#include <bits/stdc++.h>
+using namespace std;
+
+int reverse(int num)
+{
+  int d = 0, reversed = 0;
+
+  for (int i = num; i != 0; i /= 10) {
+    d = i % 10;
+    reversed *= 10;
+    reversed += d;
+  }
+
+  return reversed;
+}
+
+bool is_palindrome(int n)
+{
+  return n == reverse(n);
+}
+
+int main()
+{
+  int N, P, counter;
+  scanf("%d", &N);
+
+  while (N--) {
+    scanf("%d", &P);
+    counter = 0;
+
+    while (!is_palindrome(P + reverse(P))) {
+      P = P + reverse(P);
+      counter++;
+    }
+
+    printf("%d %d\n", counter, reverse(P));
+  }
+
+  return 0;
+}
