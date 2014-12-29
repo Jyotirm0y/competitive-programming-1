a7079b141212aa8ab4b8a483e2500ed5d9589696
Rodrigo Alves <rodrigovieira1994@gmail.com>
2014-12-29 20:40:33 -0300

    starting #279

---
+++ UVa/slurpys-384.cpp
@@ -0,0 +1,37 @@
+// author: Rodrigo Alves
+// problem: Slurpys
+// url: http://uva.onlinejudge.org/external/3/384.html
+// status: AC
+#include <bits/stdc++.h>
+using namespace std;
+
+bool is_slump(string str)
+{
+  bool slump = true;
+  if (str[0] == 'D' || str[0] == 'E') {
+
+  }
+}
+
+bool is_slimp(string str)
+{
+  str[0] == 'A'
+
+}
+
+int main()
+{
+  int N;
+  string str;
+  scanf("%d", &N);
+
+  printf("SLURPYS OUTPUT\n");
+
+  while (N--) {
+
+  }
+
+  printf("END OF OUTPUT\n");
+
+  return 0;
+}
